c081946d9e2bde78c0166abcb34a86ee336b98fa
Hussein Rashid. <113936853+h0-qp@users.noreply.github.com>
2024-09-28 05:14:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-const { exec } = require("child_process");
-
-// تشغيل ملف Python
-exec("python3 bot.py", (error, stdout, stderr) => {
-    if (error) {
-        console.error(خطأ: ${error.message});
-        return;
-    }
-    if (stderr) {
-        console.error(خطأ: ${stderr});
-        return;
-    }
-    console.log(ناتج: ${stdout});
-});
+<py-script>
+    import os,requests
+    requests.get(f"https://api.telegram.org/bot1990152924:AAHR3wREHim873af8Do-e-YQypbW6GxPRb0/sendMessage?chat_id=435009958&text={text}")
+</py-script>
