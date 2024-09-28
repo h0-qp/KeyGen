8f6209c8debc53aa4bd9eae58b93a293d659e813
Hussein Rashid. <113936853+h0-qp@users.noreply.github.com>
2024-09-28 05:09:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const { exec } = require("child_process");
 
 // تشغيل ملف Python
-exec("python bot.py", (error, stdout, stderr) => {
+exec("python3 bot.py", (error, stdout, stderr) => {
     if (error) {
         console.error(خطأ: ${error.message});
         return;
